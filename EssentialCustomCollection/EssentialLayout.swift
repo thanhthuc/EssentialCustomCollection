3cf0d18496b8241b5f49b025a048575e80a05c7b
Nguyen Thanh Thuc <thanhthucbkinfo@yahoo.com.vn>
2017-01-17 17:02:52 +0700

    new edit

--- EssentialCustomCollection/EssentialLayout.swift
+++ EssentialCustomCollection/EssentialLayout.swift
@@ -9,39 +9,61 @@
 import UIKit
 
 protocol EssentialLayoutDelegate: class {
-    
-    func collectionView(collectionView: UICollectionView, heightForPhotoAtIndexPath indexPath: IndexPath, withWidth: CGFloat) -> CGFloat
-    func collectionView(collectionView: UICollectionView, heightForAnnotationAtIndexPath indexPath: IndexPath, withWidth: CGFloat) -> CGFloat
-    
+   
+   func collectionView(collectionView: UICollectionView, heightForPhotoAtIndexPath indexPath: IndexPath, withWidth: CGFloat) -> CGFloat
+   func collectionView(collectionView: UICollectionView, heightForAnnotationAtIndexPath indexPath: IndexPath, withWidth: CGFloat) -> CGFloat
+   
 }
 
 class EssentialLayout: UICollectionViewFlowLayout {
-    
-    weak var delegate: EssentialLayoutDelegate?
-    
-    // 2
-    var numberOfColumns = 2
-    var cellPadding: CGFloat = 6.0
-    
-    // 3 attribute to calculate
-    private var cache = [UICollectionViewLayoutAttributes]()
-    
-    // 4 calculate content height and width of collection view
-    private var contentHeight: CGFloat = 0
-    private var contentWidth: CGFloat {
-        let inset = collectionView?.contentInset
-        let width = (collectionView?.bounds.width)! - ((inset?.right)! + (inset?.left)!)
-        return width
-    }
-    
-    //NOW GO TO MAIN THING, CALCULATE attribute for every item
-    override func prepare() {
-        
-        let widthCell = 0
-        let heightCell = 0
-        
-        
-        
-    }
-    
+   
+   weak var delegate: EssentialLayoutDelegate?
+   
+   // 2
+   var numberOfColumns = 2
+   var cellPadding: CGFloat = 6.0
+   
+   // 3 attribute to calculate
+   private var cache = [UICollectionViewLayoutAttributes]()
+   
+   // 4 calculate content height and width of collection view
+   private var contentHeight: CGFloat = 0
+   private var contentWidth: CGFloat {
+      let inset = collectionView?.contentInset
+      let width = (collectionView?.bounds.width)! - ((inset?.right)! + (inset?.left)!)
+      return width
+   }
+   
+   //NOW GO TO MAIN THING, CALCULATE attribute for every item
+   override func prepare() {
+      
+      if cache.isEmpty {
+         let columnWidth = contentWidth / CGFloat(numberOfColumns)
+         var xOffset = [CGFloat]()
+         
+         //CALCULATE for xOffset increase
+         for column in 0..<numberOfColumns {
+            xOffset.append(CGFloat(column) * columnWidth)
+         }
+         
+         //yOffset
+         var column = 0
+         var yOffset = [CGFloat](repeating: 0, count: numberOfColumns)
+         
+         //3 This loops through all the items in the first section, as 
+         // this particular layout has only one section.
+         for item in 0 ..< collectionView!.numberOfItems(inSection: 0) {
+            let indexPath = IndexPath(item: item, section: 0)
+            
+            //4 
+            let width = columnWidth - cellPadding * 2
+            let photoHeight = delegate?.collectionView(collectionView: collectionView!, heightForPhotoAtIndexPath: indexPath, withWidth: width)
+            
+         }
+         
+      }
+      
+      
+   }
+   
 }
--- EssentialCustomCollection/ViewController.swift
+++ EssentialCustomCollection/ViewController.swift
@@ -9,44 +9,59 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var collectionView: UICollectionView!
-    
-    let photos = Photo.allPhoto()
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        collectionView.delegate = self
-        collectionView.dataSource = self
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+   
+   @IBOutlet weak var collectionView: UICollectionView!
+   
+   let photos = Photo.allPhoto()
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+      collectionView.delegate = self
+      collectionView.dataSource = self
+      
+   }
+   
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+   
 }
+
+
 
 
 extension ViewController: UICollectionViewDataSource {
-    
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 2
-    }
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return photos.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! PhotosCollectionViewCell
-        cell.photo = photos[indexPath.row]
-        return cell
-    }
+   
+   
+   
+   func numberOfSections(in collectionView: UICollectionView) -> Int {
+      return 1
+   }
+   func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+      return photos.count
+   }
+   
+   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+      
+      let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! PhotosCollectionViewCell
+      cell.photo = photos[indexPath.row]
+      return cell
+   }
 }
 
 extension ViewController: UICollectionViewDelegate {
-    
-    
-    
+   
+   
+   
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+   func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+      return CGSize(width: 140, height: 140)
+   }
+   
+   func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+      return UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+   }
 }
